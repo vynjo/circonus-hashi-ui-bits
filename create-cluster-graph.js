65b36cba9d20222d1111e3237d62a838be85741e
Chris Madsen <madsensix@me.com>
2016-10-17 13:55:56 -0400

    Cleanup and add .js which creates both metric cluster and graph, with tags

--- create-cluster-graph.js
+++ create-cluster-graph.js
@@ -1,17 +1,14 @@
 'use strict';
 
-
-
 var stdio = require('stdio');
 var ops = stdio.getopt({
-    'cID': {key: 'c', mandatory: true, description: 'Metric Cluster ID'},
-    'token': {key: 't', mandatory: true, description: 'Circonus AP Token'},
-    'title': {args: 1, mandatory: true},
-    'tags': {key: 'o'}
+    'id': {key: 'c', args: 1, mandatory: true, description: 'Metric Cluster ID'},
+    'token': {key: 'token', args: 1, mandatory: true, description: 'Circonus AP Token'},
+    'title': {key: 'title', args: 1, mandatory: true, description: 'Graph Name'},
+    'tags': {key: 'tags', args: 1, description: 'a key/value pair of tags - i.e. "type:cluster, datacenter:aws-west"'}
 });
 
-const args = process.argv;
-console.log(args);
+/* const args = process.argv; */
 
 const api = require('circonusapi2');
 
@@ -30,9 +27,9 @@ const cluster_graph_config = {
         {
             "legend_formula": null,
             "stack": null,
-            "name": "Nomad Client Disk - Available",
+            "name": ops.title,
             "aggregate_function": "none",
-            "metric_cluster": "/metric_cluster/32468",
+            "metric_cluster": "/metric_cluster/" + ops.id,
             "axis": "l",
             "data_formula": null,
             "hidden": false
@@ -43,17 +40,13 @@ const cluster_graph_config = {
     "notes": null,
     "overlay_sets": null,
     "style": null,
-    "tags": [
-        "hs-type:client",
-        "hs-type:visualize",
-        "service:hashistack",
-        "service:nomad"
-    ],
-    "title": "Nomad Client Disk - Available TEST2"
-};
+    "tags": [ops.tags],
+    "title": ops.title};
+
+/* console.log(cluster_graph_config); */
 
 api.setup(ops.token, 'Nomad');
-api.post('/graph', cluster_graph_config, (code, error, body) => {
+const cluster_output = api.post('/graph', cluster_graph_config, (code, error, body) => {
     if (error !== null) {
         console.error(error);
         process.exit(1);
@@ -61,69 +54,4 @@ api.post('/graph', cluster_graph_config, (code, error, body) => {
     console.dir(body);
 });
 
-/*
-
-
-curl -H 'X-Circonus-App-Name: ExampleApp' \
-     -H 'X-Circonus-Auth-Token: deadbeef-badc-0ffe-edea-dbeefbadc0ff' \
-     -H 'Accept: application/json' \
-     -X POST \
-     -d @- 'https://api.circonus.com/graph'
-{
-  "datapoints": [
-    {
-      "legend_formula": null,
-      "metric_type": "numeric",
-      "check_id": 62513,
-      "stack": null,
-      "name": "Average Ping Time",
-      "data_formula": null,
-      "axis": "l",
-      "color": "#ffff00",
-      "metric_name": "average",
-      "derive": "gauge",
-      "hidden": false
-    }
-  ],
-  "title": "Ping Test"
-}
-
-
-{
-    "_cid": "/graph/50128731-033a-c050-df35-bb419ef2dbcd",
-    "access_keys": [],
-    "composites": [],
-    "datapoints": [],
-    "description": "",
-    "guides": [],
-    "line_style": null,
-    "logarithmic_left_y": null,
-    "logarithmic_right_y": null,
-    "max_left_y": null,
-    "max_right_y": null,
-    "metric_clusters": [
-        {
-            "legend_formula": null,
-            "stack": null,
-            "name": "Nomad Client Disk - Available",
-            "aggregate_function": "none",
-            "metric_cluster": "/metric_cluster/32468",
-            "axis": "l",
-            "data_formula": null,
-            "hidden": false
-        }
-    ],
-    "min_left_y": null,
-    "min_right_y": null,
-    "notes": null,
-    "overlay_sets": null,
-    "style": null,
-    "tags": [
-        "hs-type:client",
-        "hs-type:visualize",
-        "service:hashistack",
-        "service:nomad"
-    ],
-    "title": "Nomad Client Disk - Available"
-}
-*/
+console.log(cluster_output)
--- create-cluster.js
+++ create-cluster.js
@@ -1,27 +1,38 @@
 'use strict';
 
 const api = require('circonusapi2');
+var stdio = require('stdio');
+var ops = stdio.getopt({
+    'query': {key: 'c', args: 1, mandatory: true, description: 'Metric Cluster Query'},
+    'token': {key: 'token', args: 1, mandatory: true, description: 'Circonus AP Token'},
+    'title': {args: 1, args: 1, mandatory: true, description: 'Metric Cluster Name'},
+    'description': {key: 'description', args: 1, description: 'Metric Cluster Description'},
+    'tags': {key: 'tags', args: 1, description: 'a key/value pair of tags (no spaces) - i.e. type:cluster,datacenter:aws-west'}
+});
+
+const args = process.argv;
+
+var cluster_id = {};
 
 const cluster_config = {
-    "name": "Nomad Client Disk - Available",
+    "name": ops.title,
     "queries": [
       {
-        "query": "*nc-*client`host`disk*available",
+        "query": ops.query,
         "type": "average"
 
       }
     ],
-    "description": "All web requests",
-    "tags": [
-      "service:hashistack"
-    ]
+    "description": ops.description,
+    "tags": ops.tags
 };
 
-api.setup('7339c41b-48ab-617e-8859-c60ab96edb90', 'Nomad');
-api.post('/metric_cluster', cluster_config, (code, error, body) => {
+api.setup(ops.token, 'Nomad');
+
+cluster_id = api.post('/metric_cluster', cluster_config, (code, error, body) => {
     if (error !== null) {
         console.error(error);
         process.exit(1);
     }
-    console.dir(body);
+    return body;
 });
