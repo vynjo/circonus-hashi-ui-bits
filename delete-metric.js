c6af00cbfe0b0e66e6d990a5f62dccba4b0dbcd9
Chris Madsen <madsensix@me.com>
2016-10-18 16:09:02 -0400

    fix tags, again

--- delete-metric.js
+++ delete-metric.js
@@ -3,38 +3,21 @@
 const api = require('circonusapi2');
 var stdio = require('stdio');
 var ops = stdio.getopt({
-    'query': {key: 'c', args: 1, mandatory: true, description: 'Metric Cluster Query'},
+    'query': {key: 'c', args: 1, mandatory: true, description: 'Metric Query string'},
     'token': {key: 'token', args: 1, mandatory: true, description: 'Circonus AP Token'},
-    'title': {args: 1, args: 1, mandatory: true, description: 'Metric Cluster Name'},
-    'description': {key: 'description', args: 1, description: 'Metric Cluster Description'},
-    'tags': {key: 'tags', args: 1, description: 'a key/value pair of tags (no spaces) - i.e. type:cluster,datacenter:aws-west'}
 });
 
 const args = process.argv;
 
-var cluster_id = {};
-
-const cluster_config = {
-    "name": ops.title,
-    "queries": [
-      {
-        "query": ops.query,
-        "type": "average"
-
-      }
-    ],
-    "description": ops.description,
-    "tags": ops.tags
-};
-
 api.setup(ops.token, 'Nomad');
 
-api.post('/metric_cluster', cluster_config, (code, error, body) => {
+api.post('/metric', ops.query, (code, error, body) => {
     if (error !== null) {
         console.error(error);
         process.exit(1);
     }
-
+	console.log(body)
+/*
 	const cluster_graph_config = {
 	    "access_keys": [],
 	    "composites": [],
@@ -66,11 +49,35 @@ api.post('/metric_cluster', cluster_config, (code, error, body) => {
 	    "tags": ops.tags,
 	    "title": ops.title};
 
-	api.post('/graph', cluster_graph_config, (code, error, body) => {
+	api.post('/check_bundle_metrics', cluster_graph_config, (code, error, body) => {
 	    if (error !== null) {
 	        console.error(error);
 	        process.exit(1);
 	    }
 	    console.dir(body);
-	});
+
+	}); */
 });
+
+/*
+PUT /check_bundle_metrics/45315
+{
+  metrics: [
+    {
+      name: Honeydew`detectors`0`count,
+      status: active,
+      tags: [],
+      type: numeric,
+      units: seconds
+    },
+    {
+      name: average,
+      status: available,
+      tags: [],
+      type: numeric,
+      units: seconds
+    }
+  ]
+}
+4ca13090
+*/
