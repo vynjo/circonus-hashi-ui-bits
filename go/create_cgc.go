a06eb2748b386b4dd467360c1018a4c7edd7149c
Chris Madsen <madsensix@me.com>
2016-12-11 10:23:34 -0500

    Fixed return graph Cid

--- go/create_cgc.go
+++ go/create_cgc.go
@@ -56,6 +56,8 @@ type DatapointsDef struct {
 	Derive 			string 	`json:"derive"`
 	Hidden 			bool 	`json:"hidden"`
 }
+
+
 type graphMetricClusters struct {
 	LegendFormula 		string 		`json:"legend_formula"`
 	Stack 				int			`json:"stack"`
@@ -78,6 +80,7 @@ type clusterGraph struct {
 }
 
 type regularGraph struct {
+// 	AccessKeys 			[]interface{}			`json:"access_keys"`
 	Description 		string	 				`json:"description"`
 	Tags 				[]string 				`json:"tags"`
 	Title 				string 					`json:"title"`
@@ -85,6 +88,52 @@ type regularGraph struct {
 	Style 				string 					`json:"style"`
 	Datapoints			[]DatapointsDef			`json:"datapoints"`
 	Overlayset			[]interface{}			`json:"overlay_sets"`
+	LineStyle 			string 					`json:"line_style"`
+	LogarithmicRightY 	interface{} 			`json:"logarithmic_right_y"`
+	LogarithmicLeftY 	interface{} 			`json:"logarithmic_left_y"`
+	MaxLeftY 			interface{}				`json:"max_left_y"`
+	Notes 				interface{}				`json:"notes"`
+	MaxRightY			interface{}				`json:"max_right_y"`
+	MinLeftY			interface{} 			`json:"min_left_y"`
+// 	Guides 				[]interface{} 			`json:"guides"`
+// 	MetricClusters 		[]interface{} 			`json:"metric_clusters"`
+	MinRightY 			interface{} 			`json:"min_right_y"`
+}
+
+type returningGraph struct {
+	AccessKeys []interface{} `json:"access_keys"`
+	OverlaySets interface{} `json:"overlay_sets"`
+	Composites []interface{} `json:"composites"`
+	Style string `json:"style"`
+	Cid string `json:"_cid"`
+	Description string `json:"description"`
+	LineStyle string `json:"line_style"`
+	Tags []interface{} `json:"tags"`
+	LogarithmicRightY interface{} `json:"logarithmic_right_y"`
+	LogarithmicLeftY interface{} `json:"logarithmic_left_y"`
+	Datapoints []struct {
+		LegendFormula interface{} `json:"legend_formula"`
+		Caql interface{} `json:"caql"`
+		CheckID int `json:"check_id"`
+		MetricType string `json:"metric_type"`
+		Stack int `json:"stack"`
+		Name string `json:"name"`
+		Axis string `json:"axis"`
+		DataFormula interface{} `json:"data_formula"`
+		Color string `json:"color"`
+		MetricName string `json:"metric_name"`
+		Alpha string `json:"alpha"`
+		Derive string `json:"derive"`
+		Hidden bool `json:"hidden"`
+	} `json:"datapoints"`
+	MaxLeftY interface{} `json:"max_left_y"`
+	Notes interface{} `json:"notes"`
+	Title string `json:"title"`
+	MaxRightY interface{} `json:"max_right_y"`
+	MinLeftY interface{} `json:"min_left_y"`
+	Guides []interface{} `json:"guides"`
+	MetricClusters []interface{} `json:"metric_clusters"`
+	MinRightY interface{} `json:"min_right_y"`
 }
 
 var (
@@ -361,7 +410,7 @@ func makeCluster() (metricCluster, error) {
  	reqPath := "/metric_cluster"
 
 	clusterJSON, err := json.Marshal(cluster)
-		if err != nil {
+	if err != nil {
 		return  clusterReturn, err
  	}
 //  log.Printf("%v", string(clusterJSON))
@@ -459,9 +508,11 @@ func CreateCaqlGraph (caql caqlCheck) (returnGraph regularGraph, err error) {
  		
  	graph  := regularGraph {
 // 		Description: caql.Description,
-		Title: caql.DisplayName,
-		Style: "area",
-		Tags: 			caql.Tags,
+		Title: 		caql.DisplayName,
+		Style: 		"area",
+		LineStyle:	"stepped",
+		Tags: 		caql.Tags,
+		
 	}
 	datapoint := DatapointsDef {
 		CheckID: 		checknum,
@@ -484,7 +535,16 @@ func CreateCaqlGraph (caql caqlCheck) (returnGraph regularGraph, err error) {
 		os.Exit(1)
 // 		return graph, err
 	}
-// 	fmt.Printf("Put Response: %v\n", string(response))
+// 	fmt.Printf("Put Response: %v\n", response)
+	var responseGraph returningGraph
+	
+	foobar := json.Unmarshal(response, &responseGraph)
+	if foobar != nil {
+		log.Printf("foobar: %v\n%v\n", foobar, string(response))
+	}
+	graph.Cid = responseGraph.Cid
+// 	fmt.Printf("Put Response: %v\n", string(graph.Cid))
+// 	fmt.Printf("Put Response: %v\n", string(returnGraph))
 	return graph, err
 }
 
